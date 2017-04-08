c3d34324c2d722396a247b5fd3716dd2e0abb425
André Henrique Dantas Neves Cordeiro <ahdncordeiro@gmail.com>
2017-04-08 17:10:08 -0300

    end of part-2 of the estimote tutorial

---
+++ ios-estimote-sdk/ios-estimote-sdk/ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  ios-estimote-sdk
+//
+//  Created by André Henrique Dantas Neves Cordeiro on 08/04/17.
+//  Copyright © 2017 André Henrique Dantas Neves Cordeiro. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
---
+++ ios-estimote-sdk/ios-estimote-sdk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-estimote-sdk
+//
+//  Created by André Henrique Dantas Neves Cordeiro on 08/04/17.
+//  Copyright © 2017 André Henrique Dantas Neves Cordeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
